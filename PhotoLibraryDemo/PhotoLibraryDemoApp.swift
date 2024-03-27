5a619d76ee1b68515882e057e7879a91381bb4dd
Vishnu P <vishnu@mobiboxsoftech.com>
2024-03-27 13:40:34 +0530

    Initial Commit

---
+++ PhotoLibraryDemo/PhotoLibraryDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLibraryDemoApp.swift
+//  PhotoLibraryDemo
+//
+//  Created by Vishnu's Macbook Air on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLibraryDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
